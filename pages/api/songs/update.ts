2f2f18af09652a728d5585e4a399f8f1323b41c5
Danil Liptuga <danilliptuga@gmail.com>
2022-11-28 00:39:51 +0100

    cut songs

--- pages/api/songs/update.ts
+++ pages/api/songs/update.ts
@@ -6,7 +6,6 @@ import path from "path";
 export default function handler(req: any, res: any) {
     if (req.method === "POST") {
         const jsonDirectory = path.join(process.cwd(), 'public');
-        console.log(jsonDirectory + "/data/songs/songs.json");
         fs.readFile(jsonDirectory + "/data/songs/songs.json", 'utf8', function readFileCallback(err, data) {
             if (err) {
                 return res.status(405).send({error: err})
@@ -16,10 +15,10 @@ export default function handler(req: any, res: any) {
                 if (indexOfSong == -1)
                     return res.status(404).send({message: 'Song didn`t found!'});
                 else
-                    content[indexOfSong] = {...content[indexOfSong], listens: Number(content[indexOfSong].listens) + 1};
+                    content[indexOfSong].listens = Number(content[indexOfSong].listens) + 1;
                 fs.writeFile(jsonDirectory + "/data/songs/songs.json", JSON.stringify(content), 'utf8', err => {
                     if (err) throw err;
-                    return res.status(200).json(JSON.stringify(content));
+                    return res.status(200);
                 });
             }
         })
