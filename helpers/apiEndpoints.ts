a8dfa79695e1259dc2f04a2166793cbd067c4f01
Danil Liptuga <danilliptuga@gmail.com>
2022-11-30 23:27:25 +0100

    fix internal server error

--- helpers/apiEndpoints.ts
+++ helpers/apiEndpoints.ts
@@ -1,4 +1,4 @@
 export const ApiEndpoints = {
-    SONGS: "http://localhost:3000/data/songs/songs.json",
-    SONGS_UPDATE: "http://localhost:3000/api/songs/update"
+    SONGS: "/data/songs/songs.json",
+    SONGS_UPDATE: "/api/songs/update"
 }
